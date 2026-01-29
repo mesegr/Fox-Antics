0ae458b49513760575f2695355955628b3efb2d5
Alejandro Yañez Meseguer <aymeseguer@gmail.com>
2026-01-29 12:05:22 +0100

    Feat: Player gravity

---
+++ fox_antics/Scenes/Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+class_name Player
+
+const GRAVITY: float = 690
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	move_and_slide()
